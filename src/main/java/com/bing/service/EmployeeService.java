9a34f8045bac6ea973a990f871f138082528c344
lis7fire <las7fire@gmail.com>
2022-09-19 16:27:24 +0800

    V0.0.1.05【添加】管理后台-登录功能-去除业务层接口
    直接使用业务层类，系统较小，方便使用，后期再改回接口模式

--- src/main/java/com/bing/service/EmployeeService.java
+++ src/main/java/com/bing/service/EmployeeService.java
@@ -1,31 +1,69 @@
 package com.bing.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.bing.common.R;
+import com.bing.service.DTO.EmployeeDTO;
 import com.bing.entity.EmployeeDO;
+import com.bing.mapper.EmployeeDAO;
+import com.bing.util.MyBeanUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-public interface EmployeeService {
-/**-------------------------------------------------
- * @功能:     针对 员工 增删改查 服务
- *
- * @作者:  LiBingYan
- * @时间:  2022/9/16
- *--------------------------------------------------
- */
-    public void save();
+@Slf4j
+@Service
+public class EmployeeService {
+    @Autowired
+    private EmployeeDAO employeeDAO;
 
-    public boolean save(EmployeeDO newEmployee);
+    public void save() {
 
-    public boolean deleteById(Long bookId);
+    }
 
-    public boolean update(EmployeeDO newEmployee);
+    public boolean save(EmployeeDO newEmployee) {
+        return false;
+    }
 
-    public EmployeeDO getById(Long bookId);
+    public boolean deleteById(Long bookId) {
+        return false;
+    }
 
-    EmployeeDO verifyUsernamePassword(String username, String password);
+    public boolean update(EmployeeDO newEmployee) {
+        return false;
+    }
 
-    public List<EmployeeDO> getAll();
+    public EmployeeDO getById(Long employeeId) {
 
-    public IPage<EmployeeDO> getByPage(Integer page, Integer psize);
+        return employeeDAO.selectById(employeeId);
+    }
+
+    // 按照用户名查询员工信息
+    public EmployeeDTO verifyUsernamePassword(EmployeeDTO employeeDTOin) {
+        LambdaQueryWrapper<EmployeeDO> queryWrapper = new LambdaQueryWrapper<EmployeeDO>();
+        //      构造 queryWrapper 时，默认为 and 连接直接写，需要or时候才用 or()
+        queryWrapper.eq(EmployeeDO::getUsername, employeeDTOin.getUsername());
+        //                .eq(EmployeeDO::getPassword, employeeDTOin.getPassword())
+        //                .eq(EmployeeDO::getStatus, 1);
+        // 查询 DB时 无值就为null
+        EmployeeDO oneEmployee = employeeDAO.selectOne(queryWrapper);
+        if (oneEmployee == null) {
+            return null;
+        }
+
+        EmployeeDTO employeeDTOout = new EmployeeDTO();
+        MyBeanUtil.copyProperties(oneEmployee, employeeDTOout, null);
+        return employeeDTOout;
+    }
+
+
+    public List<EmployeeDO> getAll() {
+        return null;
+    }
+
+    public IPage<EmployeeDO> getByPage(Integer page, Integer psize) {
+        return null;
+    }
 }
---
+++ src/main/java/com/bing/service/EmployeeService_tmp.java
@@ -0,0 +1,41 @@
+package com.bing.service;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.bing.service.DTO.EmployeeDTO;
+import com.bing.entity.EmployeeDO;
+import com.bing.util.MyBeanUtil;
+
+import java.util.List;
+
+/**
+ * 系统较小，直接用业务层的实现，不要接口，后期稳定了再用
+ *
+ * @作者: LiBingYan
+ * @时间: 2022/9/19
+ */
+public interface EmployeeService_tmp {
+    /**
+     * -------------------------------------------------
+     *
+     * @功能: 针对 员工 增删改查 服务
+     * @作者: LiBingYan
+     * @时间: 2022/9/16
+     * --------------------------------------------------
+     */
+    public void save();
+
+    public boolean save(EmployeeDO newEmployee);
+
+    public boolean deleteById(Long bookId);
+
+    public boolean update(EmployeeDO newEmployee);
+
+    public EmployeeDO getById(Long bookId);
+
+    public EmployeeDTO verifyUsernamePassword(EmployeeDTO employeeDTO);
+
+    public List<EmployeeDO> getAll();
+
+    public IPage<EmployeeDO> getByPage(Integer page, Integer psize);
+}
